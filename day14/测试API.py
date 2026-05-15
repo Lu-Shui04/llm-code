51651a24ae4ab324b0ca1e6a7f328cf28d957c4a
米-露水 <17016282+m-ls04@user.noreply.gitee.com>
2026-05-15 21:43:04 +0800

    day14 : 基于OpenAi库api调用学习

---
+++ day14/测试API.py
@@ -0,0 +1,18 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="***wd_apikey***",
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+completion = client.chat.completions.create(
+    model="qwen3-max",
+    messages=[
+              {"role":"system","content":"you are a helpful assistant"},
+              {"role":"user","content":"你是谁"},
+            ],
+            stream=True
+)
+for chunk in completion:
+    print(chunk.choices[0].delta.content,end="",flush=True)
