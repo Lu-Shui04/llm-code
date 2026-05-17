32a39ee2d4b6210a793df0660557779a266965cb
米-露水 <281350384+Lu-Shui04@users.noreply.github.com>
2026-05-17 21:53:46 +0800

    day19 : Langchain学习(大语言模型，聊天模型，流式输出)

---
+++ day19/langchain调用聊天模型.py
@@ -0,0 +1,19 @@
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.messages import HumanMessage ,AIMessage ,SystemMessage
+
+model=ChatTongyi(model="qwen3-max")
+
+messages=[
+    SystemMessage(content="你是三国时代的唐人。"),
+    HumanMessage(content="接我的诗[天不佑我]"),
+    AIMessage(content="[心上挥剑]"),
+    HumanMessage(content="[今时不胜仗],下一句："),
+
+]
+
+# 调用stream流式输出
+res=model.stream(input=messages)
+
+# 聊天模型需要通过.content来获取内容
+for chunk in res:
+    print(chunk.content,end="",flush=True)
---
+++ day19/langchian大模型流式输出.py
@@ -0,0 +1,8 @@
+from langchain_community.llms.tongyi import Tongyi
+
+model=Tongyi(model="qwen-max")
+
+# invoke 一下全部输出，stream 流式输出
+res=model.stream(input="你是谁")
+for chunk in res:
+    print(chunk,end="",flush=True)
