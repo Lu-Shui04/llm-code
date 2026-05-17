4f50a966901112daf0c91da59f30ec83625da9b8
米-露水 <281350384+Lu-Shui04@users.noreply.github.com>
2026-05-17 22:09:35 +0800

    day19 : Langchain学习(大语言模型，聊天模型，stream流式输出)

---
+++ day19/langchain消息简写形式.py
@@ -0,0 +1,15 @@
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
+
+model=ChatTongyi(model="qwen3-max", streaming=True)
+
+messages=[
+    ("system","你是诗人家"),
+    ("human","写一首幽默诗"),
+    ("ai","我不想回答"),
+    ("human","不要生成诗，告诉我你为什么不想回答？")
+]
+res=model.stream(input=messages)
+
+for chunk in res:
+    print(chunk.content,end="",flush=True)
