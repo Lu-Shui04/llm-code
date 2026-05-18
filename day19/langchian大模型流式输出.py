1f4e877473b9282d66b38d3681881478b74f440e
米-露水 <281350384+Lu-Shui04@users.noreply.github.com>
2026-05-18 15:08:42 +0800

    Langchain学习(大语言模型，聊天模型，stream流式输出)

--- day19/langchian大模型流式输出.py
+++ day19/langchian大模型流式输出.py
@@ -3,6 +3,6 @@ from langchain_community.llms.tongyi import Tongyi
 model=Tongyi(model="qwen-max")
 
 # invoke 一下全部输出，stream 流式输出
-res=model.stream(input="你是谁")
+res=model.stream(input="你是谁啊")
 for chunk in res:
     print(chunk,end="",flush=True)
