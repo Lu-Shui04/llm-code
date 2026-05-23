3e2e0ca284df299c8337563d0c1217e1028f8b0c
米-露水 <281350384+Lu-Shui04@users.noreply.github.com>
2026-05-23 23:40:33 +0800

    长期会话记忆的学习

---
+++ day26/长期会话记忆.py
@@ -0,0 +1,119 @@
+import os,json
+from typing import Sequence
+
+from langchain_core.messages import message_to_dict,messages_from_dict,BaseMessage
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableWithMessageHistory
+
+
+# message_to_dict:单个消息对象（BaseMessage类实例） ->字典
+# messages_from_dict: [字典，字典...] -> [消息，消息...]
+# AIMessage,HumanMessage,SystemMessage 都是BaseMessage的子类
+
+
+class FileChatMessageHistory(BaseChatMessageHistory):
+    def __init__(self,session_id,storage_path):
+        self.session_id=session_id      #会话id
+        self.storage_path=storage_path  #不同会话id的存储文件，所在的文件夹路径
+        #完整的文件路径
+        self.file_path=os.path.join(self.storage_path,self.session_id)
+        
+        #确保文件夹是存在的
+        os.makedirs(os.path.dirname(self.file_path),exist_ok=True)
+
+    def add_message(self,messages:Sequence[BaseMessage]) ->None:
+        #Sequence序列 类似list，tuple
+        all_messages=list(self.messages)      #已有的消息列表
+        all_messages.append(messages)         #新的和已有的融合成一个list
+
+        #将数据同步写入到本地文件中
+        #类对象写入文件 -> 一堆二进制
+        #为了方便，可以将BaseMessage消息转为字典(借助json模块以json字符串写入文件)
+        #官方message_to_dict:单个消息对象（BaseMessage类实例） ->字典
+
+        # new_messages=[]
+        # for message in all_messages:
+        #     d=message_to_dict(messages)
+        #     new_messages.append(d)
+
+        new_messages=[message_to_dict(message) for message in all_messages]
+        #将数据写入文件
+        with open( self.file_path,"w",encoding="utf-8") as f:
+            json.dump(new_messages,f)
+
+    @property      #@property装饰器将messages方法变成成员属性用
+    def messages(self) -> list[BaseMessage]:
+        #当前文件内：list[字典]
+        try:
+            with open(self.file_path,"r",encoding="utf-8") as f:
+                messages_data=json.load(f)  #返回值就是：list[字典]
+                return messages_from_dict(messages_data)
+        except FileNotFoundError:
+            return[]
+    
+    #清空方法
+    def clear(self) ->None:
+        with open(self.file_path,"w",encoding="utf-8") as f:
+            json.dump([],f)
+
+
+
+
+
+model=ChatTongyi(model="qwen3-max",streaming=True)
+
+# Prompt=PromptTemplate.from_template(
+#     "你需要根据会话历史回应用户问题。对话历史：{chat_history},用户问题：{input},请回答"
+# )
+Prompt=ChatPromptTemplate.from_messages(
+    [
+         ("system","你需要根据会话历史回应用户问题。"),
+         MessagesPlaceholder("chat_history"),
+         ("human","请回答一下问题,{input}")
+
+    ]
+)
+
+# 字符串解析器
+str_parser=StrOutputParser()
+
+def print_prompt(full_prompt):
+    print("="*10,full_prompt.to_string(),"="*10)
+    return full_prompt
+
+# 创建基础链
+base_chain= Prompt | print_prompt |model | str_parser #提示词给模型，模型的输出字符串解析器解析成字符串
+
+
+def get_history(session_id):
+    return FileChatMessageHistory(session_id,"./chat_history")
+
+# 创建一个新的链，对原有的链增强功能:自动附加历史消息
+conversations_chain=RunnableWithMessageHistory(
+    # 需要四个参数
+    base_chain,     #被增强的原有的chain
+    get_history,    #通过会话id获取InMemoryChatMessageHistory
+    input_messages_key="input", #表示用户输入在模板中的占位符
+    history_messages_key="chat_history" #表示用户输入在模板中的占位符
+)
+
+
+if __name__=='__main__':
+    session_config={
+        "configurable":{
+            "session_id":"user001"
+        }
+    }
+
+    res=conversations_chain.invoke({"input":"小明有2条狗"},session_config)
+    print("第1次执行：",res)
+
+    res=conversations_chain.invoke({"input":"小张有3只猫"},session_config)
+    print("第2次执行：",res)
+
+    res=conversations_chain.invoke({"input":"总共多少个宠物"},session_config)
+    print("第3次执行：",res)
+
