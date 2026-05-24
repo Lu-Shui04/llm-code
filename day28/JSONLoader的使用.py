c171e97108d4ec85d92667d78fca47206238b0cd
米-露水 <281350384+Lu-Shui04@users.noreply.github.com>
2026-05-24 22:15:10 +0800

    JSONLoader的使用

--- day28/JSONLoader的使用.py
+++ day28/JSONLoader的使用.py
@@ -9,7 +9,14 @@ from langchain_community.document_loaders import JSONLoader
 loader=JSONLoader(
     file_path="./data/stus.json",
     jq_schema=".[].name",
-    text_content=False
+    text_content=False,
+
+    # json_lines=True     
+    # json_lines=True -> 告知JSONLoader 这是一个JSONLines文件
+    # (每一个都是一个独立的标准的JSON文件),比如：
+    # {"name": "张三", "age": 20, "gender": "男"},
+    # {"name": "李娜", "age": 22, "gender": "女"},
+    # {"name": "王伟", "age": 19, "gender": "男"}
 )
 
 document=loader.load()
